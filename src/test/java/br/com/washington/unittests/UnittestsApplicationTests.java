82b6ff6fd15577909aa58964f2a008ed6a0cfcb9
Washington <washington195luiz@gmail.com>
2025-06-01 00:00:39 -0300

    first commit

---
+++ src/test/java/br/com/washington/unittests/UnittestsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.unittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
